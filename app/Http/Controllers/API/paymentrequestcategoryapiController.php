dcd7b73f6765010f96bcd8bb82b3212ebe60b05b
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-05-08 01:46:13 -0700

    paymenttype cate

---
+++ app/Http/Controllers/API/paymentrequestcategoryapiController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\PaymentRequestCategory;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class paymentrequestcategoryapiController extends Controller
+{
+    public function index()
+    {
+        try {
+            $payment_request_categories = PaymentRequestCategory::all();
+            $data = array('payment_request_categories' => $payment_request_categories);
+            return response()->json([
+                'code' => 200,
+                'data' => $data,
+            ]);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $user = auth()->user();
+
+            $newData = new PaymentRequestCategory();
+            $newData->name = $request->input('category_name');
+            $newData->color = $request->input('category_color');
+            $newData->note = $request->input('note');
+            $newData->user_id = $user->id;
+
+            if ($newData->save()) {
+                return response()->json([
+                    'code' => 200,
+                    'message' => 'Ödeme Talebi Durumu Başarıyla Eklendi!',
+                ]);
+            } else {
+                return response()->json([
+                    'code' => 402,
+                    'message' => 'operation failed',
+                ]);
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function edit($id)
+    {
+        try {
+            $payment_request_categories = PaymentRequestCategory::where('id', '=', $id)->first();
+            return response()->json([
+                'code' => 200,
+                'data' => $payment_request_categories,
+            ]);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $user = auth()->user();
+
+            $temp['name'] = $request->input('category_name');
+            $temp['color'] = $request->input('category_color');
+            $temp['note'] = $request->input('note');
+            $temp['user_id'] = $user->id;
+
+            if (PaymentRequestCategory::where('id', '=', $id)->update($temp)) {
+                return response()->json([
+                    'code' => 200,
+                    'message' => 'Ödeme Talebi Durumu Başarıyla Güncellendi!',
+                ]);
+            } else {
+                return response()->json([
+                    'code' => 402,
+                    'message' => 'Operation failed',
+                ]);
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function destroy($id)
+    {
+
+        if (PaymentRequestCategory::where('id', $id)->count() > 0) {
+            if (PaymentRequestCategory::find($id)->delete()) {
+                return response()->json([
+                    'code' => 200,
+                    'message' => 'record deleted successfully!',
+                ]);
+            } else {
+                return response()->json([
+                    'code' => 400,
+                    'message' => 'Operation Failes!',
+                ]);
+            }
+        } else {
+            return response()->json([
+                'code' => 400,
+                'message' => 'There is no record for this ID!',
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\companiesController;
 use App\Http\Controllers\API\paymenttypesapiController;
+use App\Http\Controllers\API\paymentrequestcategoryapiController;
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -28,6 +31,12 @@ Route::GET('/paymenttypes/edit/{id}', [paymenttypesapiController::class, 'edit']
 Route::GET('/paymenttypes/destroy/{id}', [paymenttypesapiController::class, 'destroy']);
 Route::POST('/paymenttypes/store', [paymenttypesapiController::class, 'store']);
 Route::POST('/paymenttypes/update/{id}', [paymenttypesapiController::class, 'update']);
+//Payment Request Category
+Route::GET('/paymentrequestcategory', [paymentrequestcategoryapiController::class, 'index']);
+Route::GET('/paymentrequestcategory/edit/{id}', [paymentrequestcategoryapiController::class, 'edit']);
+Route::GET('/paymentrequestcategory/destroy/{id}', [paymentrequestcategoryapiController::class, 'destroy']);
+Route::POST('/paymentrequestcategory/store', [paymentrequestcategoryapiController::class, 'store']);
+Route::POST('/paymentrequestcategory/update/{id}', [paymentrequestcategoryapiController::class, 'update']);
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
