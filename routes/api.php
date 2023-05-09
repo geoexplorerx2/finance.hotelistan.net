f98b0229b6b3c07d604c043eb777cb4123baa151
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-05-09 04:52:00 -0700

    sanctum

--- routes/api.php
+++ routes/api.php
@@ -66,8 +66,8 @@ Route::POST('/creditcards/update/{id}', [creditcardapiController::class, 'update
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-// Route::group(['middleware' => ['auth:sanctum']], function () {
-//     Route::GET('/getToken', function () {
-//         dd('welcome');
-//     });
-// });
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::GET('/getToken', function () {
+        dd('welcome');
+    });
+});
