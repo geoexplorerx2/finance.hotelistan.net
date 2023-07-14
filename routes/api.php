81ab99ee1ccee185bf537acce8934172187f5eca
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-07-14 06:44:43 -0700

    statusCode-v2

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\API\CurrencyController;
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!.
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
